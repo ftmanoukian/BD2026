312873753e7de6b8da708020334f3823a9731e47
Francisco Manoukian <franciscomanoukian@MacBook-Pro-de-Francisco-2.local>
2026-06-17 10:08:47 -0300

    isint 4c

--- 4C/clase13/funcion_isint.py
+++ 4C/clase13/funcion_isint.py
@@ -5,6 +5,8 @@ def isint(valor):
   if valor[0] == '+' or valor[0] == '-':
     if valor[1:].isnumeric():
       return True
+    else:
+      return False
     
   elif valor.isnumeric():
     return True
@@ -13,7 +15,7 @@ def isint(valor):
     return False
   
 print(isint("123"))
-print(isint("+123"))
+print(isint("+12a"))
 print(isint("-123"))
 print(isint("1+23"))
 print(isint("12.3"))
